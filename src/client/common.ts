42ed428b404ed6980efbc2a083e817ccf28618dc
Liza <lizapolyudova@gmail.com>
2023-03-29 02:39:40 +0100

    cyclop tree + enlarge function

--- src/client/common.ts
+++ src/client/common.ts
@@ -1,4 +1,6 @@
 import {init} from "./shared/menu";
+import * as $ from 'jquery';
+
 
 // 'use strict';
 //
@@ -9,3 +11,18 @@ import {init} from "./shared/menu";
 
 console.log("hello");
 init();
+
+$('img[data-enlargable]').addClass('img-enlargable').click(function(){
+    var src = $(this).attr('src');
+    $('<div>').css({
+        background: 'RGBA(0,0,0,.5) url('+src+') no-repeat center',
+        backgroundSize: 'contain',
+        width:'100%', height:'100%',
+        position:'fixed',
+        zIndex:'10000',
+        top:'0', left:'0',
+        cursor: 'zoom-out'
+    }).click(function(){
+        $(this).remove();
+    }).appendTo('body');
+});
